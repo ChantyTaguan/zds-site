308a51f5c7f4585c2f3d53c91189bf017de94414
Gerard Paligot <gerard.paligot@gmail.com>
2014-11-27 22:09:17 +0100

    Adds DRF dependency and API to list members.
    Creates the submodule api in member module for all
    files used by the api and creates ProfileManager
    class to define all commons databases requests in
    views and api views.

--- zds/settings.py
+++ zds/settings.py
@@ -164,6 +164,7 @@ INSTALLED_APPS = (
     'email_obfuscator',
     'haystack',
     'munin',
+    'rest_framework',
 
     # Apps DB tables are created in THIS order by default
     # --> Order is CRITICAL to properly handle foreign keys
@@ -194,6 +195,14 @@ THUMBNAIL_ALIASES = {
         'gallery': {'size': (120, 120), 'crop': True},
         'content': {'size': (960, 960), 'crop': False},
     },
+}
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
 }
 
 if (DEBUG):
