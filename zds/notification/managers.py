63625870f66edbb1bb10f79884f21208d0434290
GerardPaligot <gerard.paligot@gmail.com>
2015-05-08 18:33:37 +0200

    feat(notif): Filters list of notifications by topic.
    1. Creates FilterMixin in utils/mixins.py file to have a generic mixin.
    2. Uses FilterMixin in NotificationListView and configure it.
    3. Adds all test cases about the filter.
    
    Closes #12

--- zds/notification/managers.py
+++ zds/notification/managers.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from django.contrib.contenttypes.models import ContentType
 
 from django.db import models
 
@@ -10,3 +11,7 @@ class NotificationManager(models.Manager):
 
     def get_unread_notifications_of(self, profile):
         return self.filter(subscription__profile=profile, is_read=False)
+
+    def filter_content_type_of(self, model):
+        content_subscription_type = ContentType.objects.get_for_model(model)
+        return self.filter(subscription__content_type__pk=content_subscription_type.pk)
